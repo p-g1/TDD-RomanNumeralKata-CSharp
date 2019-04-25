1cf7c7212b6f2682560502fcd1798b30a8ee0629
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-25 14:17:43 +0100

    Add project files.

---
+++ RomanNumeralsKata.Source/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata.Source
+{
+    public class Class1
+    {
+    }
+}
