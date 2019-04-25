31c4b656f057eb7af023097485a3281862b2ac80
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-25 14:43:08 +0100

    added first few tests and setup

---
+++ RomanNumeralsKata.Source/RomanNumerals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata.Source
+{
+    public class RomanNumerals
+    {
+        public string Convert(int arabic)
+        {
+            if (arabic == 10)
+            {
+                return "X";
+            }
+
+            if (arabic == 2)
+            {
+                return "II";
+            }
+
+            if (arabic == 3)
+            {
+                return "III";
+            }
+
+
+            return "I";
+        }
+    }
+}
---
+++ RomanNumeralsKata.Tests/RomanNumeralsShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using RomanNumeralsKata.Source;
+
+namespace RomanNumeralsKata.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsShould
+    {
+        [TestCase(1, "I")]
+        [TestCase(10, "X")]
+        [TestCase(2, "II")]
+        [TestCase(3,"III")]
+        public void NumeralConverter(int arabic, string numeral)
+        {
+            var romanNumerals = new RomanNumerals();
+            var result = romanNumerals.Convert(arabic);
+            result.Should().Be(numeral);
+        }
+    }
+}
