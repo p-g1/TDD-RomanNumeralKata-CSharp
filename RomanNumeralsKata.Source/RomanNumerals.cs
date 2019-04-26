1a364af03d213f8ec430282865039fc8aed20edd
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-26 15:15:51 +0100

    moved dictionary

--- RomanNumeralsKata.Source/RomanNumerals.cs
+++ RomanNumeralsKata.Source/RomanNumerals.cs
@@ -8,29 +8,28 @@ namespace RomanNumeralsKata.Source
 {
     public class RomanNumerals
     {
+        private Dictionary<int, string> _arabicToRoman = new Dictionary<int, string>
+        {
+            {1, "I"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+            //{11, "XI"},
+            //{12, "XII"},
+            //{13, "XIII"},
+            //{14, "XIV"},
+            //{15, "XV"},
+            //{16, "XVI"},
+            //{17, "XVII"},
+            //{18, "XVIII"},
+            {19, "XIX"},
+            //{20, "XX"}
+        };
+
         public string Convert(int arabic)
         {
-            var arabicToRoman = new Dictionary<int, string>
-            {
-                {1, "I"},
-                {4, "IV"},
-                {5, "V"},
-                {9, "IX"},
-                {10, "X"},
-                //{11, "XI"},
-                //{12, "XII"},
-                //{13, "XIII"},
-                //{14, "XIV"},
-                //{15, "XV"},
-                //{16, "XVI"},
-                //{17, "XVII"},
-                //{18, "XVIII"},
-                {19, "XIX"},
-                //{20, "XX"}
-            };
-
-
-            arabicToRoman = arabicToRoman.OrderByDescending(x => x.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
+            _arabicToRoman = _arabicToRoman.OrderByDescending(x => x.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
 
             var result = string.Empty;
 
@@ -46,7 +45,7 @@ namespace RomanNumeralsKata.Source
 
             if (arabic > 8 && arabic < 10)
             {
-                return arabicToRoman[1] + arabicToRoman[10];
+                return _arabicToRoman[1] + _arabicToRoman[10];
             }
 
             if (arabic == 10)
@@ -56,7 +55,7 @@ namespace RomanNumeralsKata.Source
 
             if (arabic > 10)
             {
-                return arabicToRoman[10] + arabicToRoman[1];
+                return _arabicToRoman[10] + _arabicToRoman[1];
             }
 
             if (arabic == 1)
@@ -65,17 +64,17 @@ namespace RomanNumeralsKata.Source
                 
             }
 
-            if (arabic / 5 >= 1) result += arabicToRoman[5];
+            if (arabic / 5 >= 1) result += _arabicToRoman[5];
             var remainder = arabic % 5;
 
             
 
             if (remainder > 3)
             {
-                return arabicToRoman[1] + arabicToRoman[5];
+                return _arabicToRoman[1] + _arabicToRoman[5];
             }
 
-            Enumerable.Range(0,remainder).ToList().ForEach( number => result += arabicToRoman[1] );
+            Enumerable.Range(0,remainder).ToList().ForEach( number => result += _arabicToRoman[1] );
 
 
             return result;
@@ -83,7 +82,7 @@ namespace RomanNumeralsKata.Source
 
         private Tuple<int, string> CalculateOneNumeral(int arabic, string existingNumerals)
         {
-            return new Tuple<int, string>(1, "I");
+            return new Tuple<int, string>(1, _arabicToRoman[1]);
         }
 
     }
